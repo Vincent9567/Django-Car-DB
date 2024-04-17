7bf8badf86f13d1ee2f74864ff796ed2fc3510ee
Vincent9567 <vrusso9567@gmail.com>
2024-04-16 21:31:53 -0400

    Back up 9:30

--- user_profile_app/serializers.py
+++ user_profile_app/serializers.py
@@ -6,6 +6,18 @@ from app_user_app.serializers import AppUserSerializer
 class UserProfileSerializer(serializers.ModelSerializer):
 
     account = AppUserSerializer(many=False)
+
     class Meta:
         model = UserProfile
         fields = ('account','street_name', 'street_number', 'zip_code', 'city')
+
+
+    def create(self, validated_data):
+
+        account_data = validated_data.pop('account')
+        new_user_profile = UserProfile.objects.create(**validated_data)
+
+        account = AppUser.objects.create(**account_data)
+        new_user_profile.add(account)
+
+        return new_user_profile
--- user_profile_app/views.py
+++ user_profile_app/views.py
@@ -5,12 +5,13 @@ from rest_framework.views import APIView, Response
 from rest_framework import status
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+import json
 
 class AllUserProfiles(APIView):
     
     def get(self, request):
         user_profiles = UserProfile.objects.order_by('pk')
-        serializer = UserProfileSerializer(app_users, many=True)  
+        serializer = UserProfileSerializer(user_profiles, many=True)  
         return Response(serializer.data)
     
     def post(self, request, response):
@@ -19,7 +20,7 @@ class AllUserProfiles(APIView):
             serializer.save()
             print(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class SelectUserProfiles(APIView):
@@ -31,13 +32,29 @@ class SelectUserProfiles(APIView):
             return UserProfile.objects.get(first_name=id)
 
     def get(self, request, id):
-        user_profile = self.get_app_user(id)
-        json_user_profile = serialize('json', ['user_profile'])
-        user_profile_serialized = json.loads(json_user_profile)
-        return Response(user_profile_serialized.data)
+        user_profile = self.get_user_profile(id)
+        json_user_profile = serialize('json', [user_profile])
+        serialized_user_profile = json.loads(json_user_profile)
+        return Response(serialized_user_profile.data)
     
+    def put(self, request, id):
+        user_profile = self.get_user_profile(id)
+        if 'street_name' in request.data:
+            user_profile.street_name = request.data['street_name']
+        if 'street_number' in request.data:
+            user_profile.street_number = request.data.street_number
+        if 'zip_code' in request.data:
+            user_profile.zip_code = request.data.zip_code
+        if 'email' in request.data:
+            user_profile.email = request.data.email
+        if 'password' in request.data:
+            user_profile.password = request.data.password
+
 
     def delete(self, request, id):
-        pass
         user_profile = self.get_user_profile(id)
-        serialized_
+        serialized_user_profile = UserProfileSerializer(user_profile, many=False)
+        first_name = serialized_user_profile.data[0]['account_id']['first_name']
+        last_name = serialized_user_profile.data[0]['account_id']['last_name']
+        user_profile.delete()
+        return Response(f"User account for {first_name} {last_name} has been deleted.")
