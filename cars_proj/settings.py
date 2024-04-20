4027f9df20c0d167488c386cb0f8a8323e925d28
Vincent9567 <vrusso9567@gmail.com>
2024-04-20 11:04:20 -0400

    Repo

--- cars_proj/settings.py
+++ cars_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-37_^qzmz3pys*-6$&djy3loh)r@h8y6m=z&+tt-b7lv!n&%sf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
